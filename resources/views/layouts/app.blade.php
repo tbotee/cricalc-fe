2e751e4d538869dcadd1d46718b64db4897043d9
Botond Tánczos <botond.tanczos@aplnexted.com>
2023-05-06 18:49:00 +0300

    import files fixed boostrap fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
-    <link href="assets/fonts/font-awesome.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}" type="text/css">
+    <link href="{{ asset('fonts/font-awesome.css') }}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/bootstrap-select.min.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/jquery.slider.min.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}" type="text/css">
@@ -26,6 +26,5 @@
 <footer>
     <!-- Include your site footer content here -->
 </footer>
-<script src="{{ asset('js/bootstrap.js') }}"></script>
 </body>
 </html>
