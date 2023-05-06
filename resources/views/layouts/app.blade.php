d908edb3e1e8e4075ae6c613599f3f3b5f3a6a8d
Botond Tánczos <botond.tanczos@aplnexted.com>
2023-05-06 18:15:07 +0300

    added welcome blade added layout added theme files

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+    <link href="assets/fonts/font-awesome.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-select.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/jquery.slider.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/owl.transitions.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>@yield('title')</title>
+</head>
+<body>
+<header>
+    <!-- Include your site header content here -->
+</header>
+<main>
+    @yield('content')
+</main>
+<footer>
+    <!-- Include your site footer content here -->
+</footer>
+<script src="{{ asset('js/bootstrap.js') }}"></script>
+</body>
+</html>
