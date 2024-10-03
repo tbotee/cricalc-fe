79e92e7412e2dbe6adfdbdd13aeb1cfa26d15504
Botond Tánczos <botond.tanczos@aplnexted.com>
2024-10-03 16:07:57 +0300

    font fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,700&display=swap" rel="stylesheet">
     <link href="{{ asset('fonts/font-awesome.css') }}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/bootstrap-select.min.css') }}" type="text/css">
